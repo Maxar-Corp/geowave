b22699cd1c51039e876eaff67c9997d0e7eb4df3
Justin Fleck <justin@strategicmissionelements.com>
2020-06-23 13:17:58 -0400

    Update deploy scripts

--- deploy-accumulo-singlejar.sh
+++ deploy-accumulo-singlejar.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-mvn package -P geowave-tools-singlejar -Dfindbugs.skip -Dspotbugs.skip -DskipTest -Djava.awt.headless=true
+mvn package -P accumulo-container-singlejar -Dfindbugs.skip -Dspotbugs.skip -DskipTest -Djava.awt.headless=true
 
 mvn deploy:deploy-file -DgroupId=org.locationtech.geowave \
-  -DartifactId=geowave-tools-singlejar \
+  -DartifactId=geowave-accumulo-singlejar \
   -Dversion=1.2.0-SNAPSHOT \
   -Dpackaging=jar \
-  -Dfile=deploy/target/geowave-deploy-1.2.0-SNAPSHOT-tools.jar \
+  -Dfile=deploy/target/geowave-deploy-1.2.0-SNAPSHOT-accumulo.jar \
   -DrepositoryId=safehouse-maven-snapshot \
   -Durl=http://ec2-3-80-38-6.compute-1.amazonaws.com/repository/safehouse-maven-snapshot \
   -Djava.awt.headless=true
